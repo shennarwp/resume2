cfbec1c4776a980cee7f928cae02bfa4321a3abf
Shenna Risqianto Wilfred Piri <shenna.piri@fourenergy.de>
2026-07-10 15:02:39 +0200

    adjust color lang switch dark mode

--- src/lib/components/LanguageSwitcher.svelte
+++ src/lib/components/LanguageSwitcher.svelte
@@ -7,9 +7,9 @@
 </script>
 
 <div class="language-switcher">
-  <button onclick={() => setLocale('de')} class:active={$locale === 'de'}><b>DE</b></button>
-  <button onclick={() => setLocale('en')} class:active={$locale === 'en'}><b>EN</b></button>
-  <button onclick={() => setLocale('id')} class:active={$locale === 'id'}><b>ID</b></button>
+  <button onclick={() => setLocale('de')} class:active={$locale === 'de'}><b>de</b></button>
+  <button onclick={() => setLocale('en')} class:active={$locale === 'en'}><b>en</b></button>
+  <button onclick={() => setLocale('id')} class:active={$locale === 'id'}><b>id</b></button>
 </div>
 
 <style>
@@ -23,19 +23,19 @@
   .language-switcher button {
     background: none;
     border: none;
-    padding: 2px 3px;
+    padding: 4px 4px;
     cursor: pointer;
-    color: var(--text-color, #333);
+    color: var(--language-switcher-button-text-color, #333);
     transition:
       background-color 0.2s,
       color 0.2s;
-    font-size: 0.6em;
+    font-size: 0.8em;
   }
   .language-switcher button:hover {
-    background-color: var(--button-hover-bg, #e0e0e0);
+    background-color: var(--language-switcher-button-hover-bg, #e0e0e0);
   }
   .language-switcher button.active {
-    background-color: var(--primary-color, #404040);
-    color: white;
+    background-color: var(--language-switcher-button-active-bg, #404040);
+    color: var(--language-switcher-button-active-text-color, white);
   }
 </style>
