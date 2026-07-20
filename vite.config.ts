c85fed223c737eb61ea0f749c7207262e372aed0
Shenna Risqianto Wilfred Piri <shenna.piri@fourenergy.de>
2026-07-20 14:56:32 +0200

    fix package lock

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+﻿import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
@@ -6,23 +6,25 @@ export default defineConfig({
   plugins: [
     sveltekit({
       compilerOptions: {
-        // Force runes mode for the project, except for libraries. Can be removed in svelte 6.
         runes: ({ filename }) =>
           filename.split(/[/\\]/).includes('node_modules') ? undefined : true,
       },
-
-      // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-      // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-      // See https://svelte.dev/docs/kit/adapters for more information about adapters.
       adapter: adapter({
-        // default options are shown. On some platforms
-        // these options are set automatically — see below
         pages: 'build',
         assets: 'build',
-        fallback: '404.html', // dependent on your host, for SPA mode use 'index.html'
+        fallback: '404.html',
         precompress: false,
         strict: true,
       }),
     }),
   ],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+    },
+    cssMinify: true,
+  },
 });
